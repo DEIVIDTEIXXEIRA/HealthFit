208e0fa50830a0441fc9f222a652bd1fc63acf7e
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-10-18 14:10:46 -0300

    FEAT(Configurar): Rotas sendo configuradas e colacadas dentro do router.

--- API/src/Router/rotas/rotas.go
+++ API/src/Router/rotas/rotas.go
@@ -1,11 +1,26 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
 
-//Rota representa o formato das rotas utilizadas na API
+	"github.com/gorilla/mux"
+)
+
+// Rota representa o formato das rotas utilizadas na API
 type Rota struct {
 	URI                string
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Configurar coloca todas as rotas dentro do router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotaUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r 
 }
