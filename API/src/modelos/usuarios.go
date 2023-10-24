6a47ef25eff31ec2475a97edf0c4dfe13aab5a45
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-10-24 14:21:28 -0300

    FEAT(Usuários): Validação. - Formatando, validando e preparando as informações do usuários antes de enviar para o repositório.

--- API/src/modelos/usuarios.go
+++ API/src/modelos/usuarios.go
@@ -1,5 +1,10 @@
 package modelos
 
+import (
+	"errors"
+	"strings"
+)
+
 // Usuario representa um usuario da aplicação
 type Usuario struct {
 	Id     uint64 `json:"id,omitempty"`
@@ -10,4 +15,45 @@ type Usuario struct {
 	Altura uint64 `json:"altura,omitempty"`
 	Idade  uint64 `json:"idade,omitempty"`
 	Senha  string `json:"senha,omitempty"`
+}
+
+// Preparar formata e verifica espaçoes em branco em campos obrigatórios.
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil 
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O campo Nome é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O campo Nick é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Email == "" {
+		return errors.New("O campo Email é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Senha == "" {
+		return errors.New("O campo Senha é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Peso == 0 {
+		return errors.New("O campo Peso é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Altura == 0 {
+		return errors.New("O campo Altura é obrigatório e não pode estar vazio!!")
+	}
+	if usuario.Idade == 0 {
+		return errors.New("O campo Idade é obrigatório e não pode estar vazio!!")
+	}
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)  
 }
