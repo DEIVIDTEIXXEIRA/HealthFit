82721566753018007de56e153093da7e3b52b706
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-11-22 21:03:02 -0300

    FEAT(Autenticação): Token sendo validado. - Função que extrai o token. - Função que verifica a chave de assinatura. - Função que valida sendo chamada no middlewares.

--- API/src/middlewares/middlewares.go
+++ API/src/middlewares/middlewares.go
@@ -1,7 +1,8 @@
 package middlewares
 
 import (
-	"fmt"
+	"API/src/autenticacao"
+	"API/src/respostas"
 	"log"
 	"net/http"
 )
@@ -16,7 +17,10 @@ func Logger(next http.HandlerFunc) http.HandlerFunc {
 // Autenticar verifica se o usuário esta autenticado para executar a rota solicitada.
 func Autenticar(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Autenticando")
+		if erro := autenticacao.ValidarToken(r); erro != nil {
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
 		next(w, r)
 	}
 }
